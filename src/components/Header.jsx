b4c23d8413d841e9f2083d02d038fca89546114b
akshatmittal61 <akshatmittal2506@gmail.com>
2022-01-14 15:45:51 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import favicon from "../images/favicon.svg";
+
+const Header = () => {
+	const [rotate, setRotate] = useState(true);
+	setTimeout(() => {
+		setRotate(!rotate);
+	}, 2000);
+	return (
+		<header className="header">
+			<div className="header-left">
+				<div className="header-left-burger">
+					<button className="icon">
+						<span className="material-icons">menu</span>
+					</button>
+				</div>
+				<div className="header-left-logo">
+					<div className="header-left-logo-image">
+						<img
+							src={favicon}
+							alt="Star Wars"
+							className="header-left-logo-image__img"
+							style={{
+								transform: `rotateY(${rotate ? 0 : 180}deg)`,
+							}}
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="header-right"></div>
+		</header>
+	);
+};
+
+export default Header;
